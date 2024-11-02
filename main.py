e6fa4e82700f058e156a2563abe109e5dd16a364
czx04 <dhit2k4@gmail.com>
2024-11-02 13:59:57 +0700

    first

---
+++ main.py
@@ -0,0 +1,216 @@
+import dash
+from altair.examples.pyramid import color
+from dash import dcc, html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+from pydeck.bindings.map_styles import styles
+from skimage.color.rgb_colors import white
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# Layout
+main_layout = html.Div(
+    className="app",
+    children=[
+        html.Div(
+            className="navbar",
+            children=[
+                html.H1("Streaming Dashboard Game", style={"color": "white", "margin": 0}),
+                html.Div(
+                    className="navbar-icons",
+                    children=[
+                        html.Img(
+                            src="https://stream-metrics-b5f68d63e431.herokuapp.com/assets/github-mark-white.png",
+                            alt="GitHub",
+                            width="24",
+                            height="24",
+                            style={"cursor": "pointer"},  # Moved cursor styling here
+                        ),
+                        html.Img(
+                            src="./assets/IMG/info_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg",
+                            alt="Info",
+                            className="info-icon",
+                            width="24",
+                            height="24",
+                        ),
+                    ],
+                    style={"display": "flex", "justify-content": "center", "align-items": "center", "gap": "10px"},
+
+                ),
+            ],
+            style={
+                "display": "flex",
+                "justifyContent": "space-between",
+                "alignItems": "center",
+                "backgroundColor": "#1e1e1e",
+                "padding": "10px 20px",
+                "borderBottom": "1px solid #d5d5d5",
+                "marginBottom": "20px",
+                "width": "100%",
+                "padding-bottom": "5px",
+            }
+        ),
+        html.Div(
+            className="data-from",
+            children=[
+                html.P("Data for this app is pulled from the FK company API. Updated 2024-10-18",
+                       style={"textAlign": "right", "padding-right": "40px"}, )
+
+            ],
+            style={
+
+            }
+        ),
+        html.Button(
+            className="filter-container",
+            children=[
+                html.P("Filters", style={"font-family": "Roboto", "font-weight": "500", "font-size": "16px",
+                                         "padding-top": "10px"}, ),
+                html.Img(
+                    src="./assets/IMG/outline.svg",
+                    alt="GitHub",
+                    style={"cursor": "pointer", "transform": "rotate(270deg)"},  # Moved cursor styling here
+                )
+            ],
+            style={
+                "display": "flex",
+                "justifyContent": "space-between",
+                "alignItems": "center",
+                "width": "85%",
+                "padding": "0px 20px"
+            }
+        ),
+        html.Div(
+            className="card-container",
+            children=[
+                html.Div(
+                    className="card",
+                    children=[
+                        html.P("37",style={
+                            "textAlign": "left",
+                            "font-family": "Roboto",
+                            "font-weight": "700",
+                            "font-size": "45px",
+                            "padding-top": "10px",
+                            "padding-left": "10%",
+                            "margin-bottom": "0px",
+                        }),
+                        html.P("IOS",style={
+                            "textAlign": "left",
+                            "font-family": "Roboto",
+                            "font-size": "18px",
+                            "font-weight": "700",
+                            "padding-left": "10%",
+                        }),
+                    ],
+                    style={
+                        "width": "22%",
+
+                    }
+                ),
+html.Div(
+                    className="card",
+                    children=[
+                        html.P("37",style={
+                            "textAlign": "left",
+                            "font-family": "Roboto",
+                            "font-weight": "700",
+                            "font-size": "45px",
+                            "padding-top": "10px",
+                            "padding-left": "10%",
+                            "margin-bottom": "0px",
+                        }),
+                        html.P("ANDROID",style={
+                            "textAlign": "left",
+                            "font-family": "Roboto",
+                            "font-size": "18px",
+                            "font-weight": "700",
+                            "padding-left": "10%",
+                        }),
+                    ],
+                    style={
+                        "width": "22%",
+
+                    }
+                ),
+html.Div(
+                    className="card",
+                    children=[
+                        html.P("37",style={
+                            "textAlign": "left",
+                            "font-family": "Roboto",
+                            "font-weight": "700",
+                            "font-size": "45px",
+                            "padding-top": "10px",
+                            "padding-left": "10%",
+                            "margin-bottom": "0px",
+                        }),
+                        html.P("MACOS",style={
+                            "textAlign": "left",
+                            "font-family": "Roboto",
+                            "font-size": "18px",
+                            "font-weight": "700",
+                            "padding-left": "10%",
+                        }),
+                    ],
+                    style={
+                        "width": "22%",
+
+                    }
+                ),
+html.Div(
+                    className="card",
+                    children=[
+                        html.P("37",style={
+                            "textAlign": "left",
+                            "font-family": "Roboto",
+                            "font-weight": "700",
+                            "font-size": "45px",
+                            "padding-top": "10px",
+                            "padding-left": "10%",
+                            "margin-bottom": "0px",
+                        }),
+                        html.P("WINDOWS",style={
+                            "textAlign": "left",
+                            "font-family": "Roboto",
+                            "font-size": "18px",
+                            "font-weight": "700",
+                            "padding-left": "10%",
+                        }),
+                    ],
+                    style={
+                        "width": "22%",
+
+                    }
+                )
+            ],
+            style={
+                "width": "85%",
+                "display": "flex",
+                "alignItems": "center",
+                "justifyContent": "space-between",
+            }
+        ),
+
+    ],
+    style={
+        "width": "100%",
+        "display": "flex",
+        "flex-direction": "column",
+        "alignItems": "center",
+        "gap": "10px",
+    }
+)
+
+addlayout = html.Div(
+
+)
+
+
+
+#all layout
+app.layout = html.Div(children=[main_layout, addlayout])
+# Run app
+if __name__ == "__main__":
+    app.run_server(debug=True)
