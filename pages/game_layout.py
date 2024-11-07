8f113f2c9104776b28a9d996e44aa819528658eb
czx04 <dhit2k4@gmail.com>
2024-11-08 00:33:33 +0700

    update8/11p1

--- pages/game_layout.py
+++ pages/game_layout.py
@@ -1,6 +1,7 @@
 # game_layout.py
 import plotly.graph_objs as go
 from dash import html, dcc
+from openpyxl.styles.builtins import title
 
 from PYTHON.data import datagame, months
 
@@ -29,13 +30,22 @@ def graph_nav(game_path):
         x=months,
         y=user_counts,
         mode='lines+markers',
-        name=game_data["game"]
+        name=game_data["game"],
+        line=dict(color='white', shape='spline', smoothing=1.3),  # Đường vẽ màu trắng
+        fill='tonexty',  # Tạo vùng nền mờ dưới đường vẽ
+        fillcolor='rgba(255, 255, 255, 0.2)',  # Màu nền mờ (trắng, độ mờ 0.2)
+        marker=dict(size=8)
     )
     fig = go.Figure(data=[trace])
     fig.update_layout(
-        title='User Growth for Valorant Over Months',
-        xaxis_title='Month',
-        yaxis_title='Number of Users',
+        title={
+            'text': 'User Growth Over the Year',
+            'x': 0.06,
+            'xanchor': 'left',  # Cố định vị trí tiêu đề từ bên trái
+            'yanchor': 'top'  # Giữ tiêu đề nằm ở trên
+        },
+        paper_bgcolor='rgba(0,0,0,0)',  # Làm nền biểu đồ trong suốt để lộ gradient phía sau
+        plot_bgcolor='rgba(0,0,0,0)',  # Làm nền khu vực plot trong suốt
         template='plotly_dark'
     )
     return html.Div(
@@ -43,9 +53,10 @@ def graph_nav(game_path):
         children=[
             html.Div(
                 children=[
-                    dcc.Graph(figure=fig)
+                    dcc.Graph(figure=fig,config={"displayModeBar": False}),
+
                 ],
-                style={"padding": "20px"}
+                style={"padding": "20px","background": "linear-gradient(to right, #0c2442, #2a5687)"}
             )
         ],
         style={},
